92b88a70186ea55eb32f33eb88dfcbd3bfdd74cc
libbliy <haha@163.com>
2018-02-01 12:35:56 +0800

    table create

---
+++ app/src/main/java/com/example/pururin/coolweather/db/City.kt
@@ -0,0 +1,19 @@
+package com.example.pururin.coolweather.db
+
+import org.litepal.crud.DataSupport
+
+/**
+ * Created by libbliy on 2018/1/30.
+ */
+class City : DataSupport() {
+    var id: Int? = null
+    var mCityName:String?=null
+    var mCityCode:Int?=null
+
+    var mProvinceId:Int?=null
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/pururin/coolweather/db/County.kt
@@ -0,0 +1,17 @@
+package com.example.pururin.coolweather.db
+
+import org.litepal.crud.DataSupport
+
+/**
+ * Created by libbliy on 2018/1/30.
+ */
+class County : DataSupport() {
+    var id: Int? = null
+    var mCountyName:String?=null
+    var mWeatherId:String?=null
+    var mCityId:Int?=null
+
+
+
+
+}
